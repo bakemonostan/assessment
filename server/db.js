668835aa0a7cf822ff590fa23bd77423eec8ea91
bakemonostan <ehizomokhomion@gmail.com>
2024-03-24 10:35:22 +0100

    updated

--- server/db.js
+++ server/db.js
@@ -1,13 +1,13 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-// let localPoolConfig = {
-//   user: 'postgres',
-//   password: 'edialeomo12',
-//   host: 'localhost',
-//   port: '5432',
-//   database: 'hyperislandtodoapp'
-// };
+let localPoolConfig = {
+  user: 'postgres',
+  password: 'edialeomo12',
+  host: 'localhost',
+  port: '5432',
+  database: 'hyperislandtodoapp'
+};
 
 const poolConfig = process.env.DATABASE_URL ? {
   connectionString: process.env.DATABASE_URL,
