98a918905a738ef2727f08623dd69ad7646f40f6
bakemonostan <ehizomokhomion@gmail.com>
2024-03-24 10:21:04 +0100

    update

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
@@ -12,7 +9,6 @@ import authRoutes from './routes/auth-routes.js'
 dotenv.config();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-// const prisma = new PrismaClient();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
